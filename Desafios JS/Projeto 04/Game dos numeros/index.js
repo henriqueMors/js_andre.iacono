535f8b1d63b8027ee6ebd88a23bebd871713b806
Henrique Moraes <ahmdesouza@gmail.com>
2022-12-28 21:16:50 -0300

    update

--- Desafios JS/Projeto 04/Game dos numeros/index.js
+++ Desafios JS/Projeto 04/Game dos numeros/index.js
@@ -21,19 +21,19 @@ function compareNumbers() {
 
     if (attempts < maxguesses) {
         if (userNumber > computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('textOutput').innerHTML = 'Seu número é muito alto'
             document.getElementById('inputBox').value = ''
             ++attempts
             document.getElementById('attempts').innerHTML = attempts
            }
            else if (userNumber < computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too low'
+            document.getElementById('textOutput').innerHTML = 'Seu número é muito baixo'
             document.getElementById('inputBox').value = ''
             ++attempts
             document.getElementById('attempts').innerHTML = attempts
            }
            else {
-            document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+            document.getElementById('textOutput').innerHTML = 'Parabéns!!!'
             ++attempts
             document.getElementById('attempts').innerHTML = attempts
             document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
